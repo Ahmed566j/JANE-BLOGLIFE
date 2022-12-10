3fd3dcc07d12ed34f66ca2ae5d4c4399b36564ce
Ahmed_Ibrahim <105848035+Ahmed566j@users.noreply.github.com>
2022-12-10 15:12:24 +0200

    Add files via upload

---
+++ Js/Style.js
@@ -0,0 +1,27 @@
+var x = document.getElementById('comment-div')
+var x1 = document.getElementById('comment-div1')
+var x2 = document.getElementById('comment-div2')
+
+function show(){
+    if(x.style.display == 'block'){
+        x.style.display= 'none';
+        } else{
+            x.style.display= 'block';
+        }
+}
+
+function show1(){
+    if(x1.style.display == 'block'){
+        x1.style.display= 'none';
+        } else{
+            x1.style.display= 'block';
+        }
+}
+
+function show2(){
+    if(x2.style.display == 'block'){
+        x2.style.display= 'none';
+        } else{
+            x2.style.display= 'block';
+        }
+}
